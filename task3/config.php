cd4e637e822527c40c6d7b3445f900297d3855d0
constalink35 <constalink@gmail.com>
2020-03-04 11:02:38 +0400

    commit 1

--- task3/config.php
+++ task3/config.php
@@ -1,9 +1,17 @@
 <?php
-
+ini_set( "display_errors", true );
+date_default_timezone_set("Europe/Samara");
 
 define("DATABASE_HOST", "localhost");
 define("DATABASE_NAME", "task3");
 define("DATABASE_USERNAME", "root");
 define("DATABASE_PASSWD", "");
+
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+
+set_exception_handler( 'handleException' );
 
 ?>
